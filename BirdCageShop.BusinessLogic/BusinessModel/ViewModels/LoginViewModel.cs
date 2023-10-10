e0bce25ed5fd0702a463c959fc97143a0821521a
phamduchuy284 <huypd284@gmail.com>
2023-10-10 13:08:43 +0700

    Login API fix

--- BirdCageShop.BusinessLogic/BusinessModel/ViewModels/LoginViewModel.cs
+++ BirdCageShop.BusinessLogic/BusinessModel/ViewModels/LoginViewModel.cs
@@ -3,8 +3,13 @@
     public class LoginViewModel
     {
         public string UserId { get; set; } = null!;
-        public string? Email { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public int? RoleId { get; set; }
+        public int? Status { get; set; }
     }
 }
