ea0111fd2c6624168b1adcee09348cf097949447
Pham Duc Huy <139197400+huyphamduc284@users.noreply.github.com>
2023-11-05 11:20:09 +0700

    replace zipcode

--- BirdCageShop.BusinessLogic/BusinessModel/ViewModels/OrderViewModel.cs
+++ BirdCageShop.BusinessLogic/BusinessModel/ViewModels/OrderViewModel.cs
@@ -12,7 +12,7 @@ namespace Ecommerce.BusinessLogic.ViewModels
         public string? OrderStatus { get; set; }
         public string? Address { get; set; }
         public string? State { get; set; }
-        public string? ZipCode { get; set; }
+        public string? Voucher { get; set; }
         public string? Country { get; set; }
         public string? Method { get; set; }
         public string? Comment { get; set; }
